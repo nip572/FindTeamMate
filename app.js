aa3e3bc9a6d352080e4055e5e55baac56ec3c8c5
nip572 <nipun.ahuja572@gmail.com>
2017-02-13 00:16:38 -0800

    modal for create account added. create user is disabled

--- app.js
+++ app.js
@@ -43,27 +43,21 @@ TeamApp.controller('AuthCtrl' ,['$rootScope','$scope', '$window','Auth','$locati
        $location.path(path);
    };
 
-   $scope.user = function () {
+   $scope.authChanged = function () {
        console.log("in Auth Changed");
        Auth.$onAuthStateChanged( function (user) {
-           if(user){
+
                console.log("Logged in as: " + user.uid);
                $scope.uid = user.uid;
                $scope.go("/"+$scope.uid);
-               $rootScope.userRef = firebase.database().ref();
-               console.log($rootScope.userRef+"   uder ref");
-               $rootScope.userObject = $firebaseObject($rootScope.userRef.child(user.uid));
-               if($rootScope.userObject === null){
-                   console.log("userRef is Null");
-               }
-               else{
-                   console.log("rootscope "+$rootScope.userObject.$values);
-               }
-
+               const dbRef = firebase.database().ref();
+               const userRef = dbRef.child(user.uid);
+               $scope.object = $firebaseObject(userRef) ;
+               console.log($scope.object);
 
                //make database model
 
-           }
+
        });
    };
 
@@ -71,18 +65,30 @@ TeamApp.controller('AuthCtrl' ,['$rootScope','$scope', '$window','Auth','$locati
         console.log("In Login Method");
         Auth.$signInWithEmailAndPassword($scope.email , $scope.password).then(function (auth) {
             console.log("Logged in successfully");
-            $scope.user();
-            //$window.location.href = 'pages/home.htm';
+            $scope.authChanged();
 
         }, function (error) {
+
+
                 console.log("Error Occured" + error.message);
             });
     };
 
     $scope.createUser = function () {
+
+       $scope.user.uid = {
+           "email":"",
+           "name":"",
+           //COMPLETE THIS HOOK IT UPTO MODEL IN MODAL. CREATE USER IS DISCONNECTED 
+
+       }
         console.log("In create Method");
         Auth.$createUserWithEmailAndPassword($scope.email , $scope.password).then(function (auth) {
             console.log("User Created Successfully");
+            //OPEN FORM AND ADD SKILL
+
+
+
             $scope.login();
         }, function (error) {
             console.log("Error Occured" + error.message);
